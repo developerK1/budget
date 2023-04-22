f9cd1b14fa49fca752ea0ac3d6af158c4b6cf3ac
Kekaone <maobaKG@yahoo.com>
2023-04-22 20:49:24 +0200

    just pushing bakcend

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,24 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const transactionRoutes = require('./routes/transaction.route');
 const port = process.env.PORT || 4040;
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-// const uri = preocess.env.DB_URI;
-// mongoose.connect(uri, {userUrlParser :true, useCreateIndex :true}));
-// const connection = mongoose.connection;
+const uri = process.env.DB_URI;
+mongoose.connect("mongodb+srv://maobakg:pWResSro3qdalaGq@cluster0.uidjjph.mongodb.net/FinancialLog");
+//mongoose.connect('mongodb://127.0.0.1:27017/FinancialLog');
+const connection = mongoose.connection;
 
-// connection.once('open', ()=>{
-// 	console.log("Mongoose reached the deb : CONNECTED..'");
-// })
+connection.once('open', ()=>{
+	console.log("MongoDB database connection established successfully");
+})
 
-
-app.get(/, (req, res )=>{
-    res.json("This is home page");
-} )
+app.use('/transaction', transactionRoutes);
 
 
 app.listen(port,()=>{
