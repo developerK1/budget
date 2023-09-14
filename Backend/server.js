4e54a496a736ea80c6ce2537d8b4c0f32c0d403b
Dev Kekeaone <83980761+developerK1@users.noreply.github.com>
2023-09-14 16:18:20 +0200

    Update server.js with CORS configs

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,40 @@
-const express = require("express");
+preventing const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const transactionRoutes = require('./routes/transaction.route');
 const port = process.env.PORT || 4040;
 
-app.use(cors());
+
+const corsOptions = {
+  origin: 'https://budget-app-blush.vercel.app/',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Enable cookies and HTTP authentication
+  optionsSuccessStatus: 204, // Respond to preflight requests with 204 No Content
+  allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+};
+
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 const uri = process.env.DB_URI;
-mongoose.connect("mongodb+srv://maobakg:pWResSro3qdalaGq@cluster0.uidjjph.mongodb.net/FinancialLog");
-//mongoose.connect('mongodb://127.0.0.1:27017/FinancialLog');
+//mongoose.connect("mongodb+srv://maobakg:pWResSro3qdalaGq@cluster0.uidjjph.mongodb.net/FinancialLog");
+mongoose.connect(uri);
 const connection = mongoose.connection;
 
+
 connection.once('open', ()=>{
-	console.log("MongoDB database connection established successfully");
+	console.log("MongoDB database connection established successfully ");
 })
+
+// Allow requests from all domains
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.use('/transaction', transactionRoutes);
 
